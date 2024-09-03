7ce39a900cc94875b68e11e8c89174ffe335c562
lordaun <julur2005@gmail.com>
2024-09-03 14:11:38 +0700

    dobavleno peremenniye and primety deleniya

--- pr001.java
+++ pr001.java
@@ -1,5 +1,12 @@
 class pr001 {
 	public static void main(String args[]) {
 		System.out.println("first programm");
+		//vivodim stroku na console
+		int Var1 = 1024;
+		int Var2 = 411;
+		// broken System.out.println(Var2);
+		System.out.println("rezult chetnoe " + Var1/2);
+		System.out.println("rezult nechetnoe " + Var2/2);
+		double Var3 = (double)Var2/2;
 	}	
 }
