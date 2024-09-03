cf6bb23c9248d1a8e7f28e7299e8882dc74dead0
lordaun <julur2005@gmail.com>
2024-09-03 14:41:39 +0700

    Dobavlini uslovniye operatori and operatori chikla

--- pr001.java
+++ pr001.java
@@ -8,5 +8,21 @@ class pr001 {
 		System.out.println("rezult chetnoe " + Var1/2);
 		System.out.println("rezult nechetnoe " + Var2/2);
 		double Var3 = (double)Var2/2;
+//		if(Var1 == 0)
+//			System.out.println("ez gotovo");
+		if(Var1/2 == 512)
+                        System.out.println("uslovie Var1/2=512 vipolneno");
+		if(true)
+			System.out.println("uslovie true vipolneno");
+		System.out.println("stroka proverki");
+		for (int i=0; i<10; i++)
+			System.out.println("Stroka nomer " + i);
+//		i = 10;
+   		System.out.println();
+		int j;
+		for (j=0; j<10; j++)
+                        System.out.print(j + " ");
+		System.out.println();
+
 	}	
 }
