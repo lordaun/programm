13e83b908bffe7d3b3a153636c638972e3401c7a
lordaun <julur2005@gmail.com>
2024-09-24 13:40:45 +0700

    Добавлен цикл while

--- pr003.java
+++ pr003.java
@@ -126,6 +126,29 @@ class pr003{
 	int sum = 0;
 	for(i8 = 1; i8 <= 5; sum += i8++);
 	System.out.println("Сумма чисел равна " +sum);
+	
+	System.out.println("\n");
+	ch = 'а';
+	while (ch <= 'я') {
+		System.out.print(ch);
+		ch++;
+	}
+	System.out.println();
+
+	System.out.println("\n");
+
+	int e;
+	int result;
+	for (int i9=0; i9 < 10; i9++) {
+		result = 1;
+		e = i9;
+		while(e > 0) {
+			result *=2;
+			e--;
+		}
+		System.out.println("2 в степени " + i9 + " равно " + result);
+	}
+
 	}
 }
 
