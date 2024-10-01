780fda7353336451b77150b60b952b4dd1c017e9
lordaun <julur2005@gmail.com>
2024-10-01 15:27:56 +0700

    Добавили метод вычисления расстояния в класс

--- pr004.java
+++ pr004.java
@@ -2,6 +2,10 @@ class Avtomobil {
 	int pass;
 	int v;
 	double rash;
+
+	void rasst() {
+		System.out.println("Расстояние на полном баке " + ((int)(v/rash *100)));
+	}
 }
 class pr004 {
 	public static void main(String args[]) {
@@ -11,16 +15,28 @@ class pr004 {
 
 		lada.pass = 4;
 		lada.v = 40;
-		lada.rash = 9.5;
+		lada.rash = 5.5;
 
 		porshe.pass = 1;
 		porshe.v = 100;
 		porshe.rash = 14.8;
 
-		rasst = (int) (lada.v/lada.rash *100);
-		rasst2 = (int) (porshe.v/porshe.rash *100);
+		/*
+		Avtomobil mercedes;
 
-		System.out.println("Lada проедет " + rasst + "км. на полном баке");
-		System.out.println("Porshe  проедет " + rasst2 + "км. на полном баке");
+		mercedes = porshe;
+		System.out.println("rash для Mercedes: " + mercedes.rash);
+		mercedes.rash = 18.0;
+		System.out.println("rash для Porshe: " + porshe.rash);
+		*/
+
+		//rasst = (int) (lada.v/lada.rash *100);
+		//rasst2 = (int) (porshe.v/porshe.rash *100);
+
+		System.out.print("Для Lada: ");
+		lada.rasst();
+
+		System.out.print("Для Porshe");
+		porshe.rasst();
 	}
 }
