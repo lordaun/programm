c45bbcced56a758a72ec0177300add1da94a02d0
lordaun <julur2005@gmail.com>
2024-10-01 15:06:23 +0700

    Добавили новый класс Porshe

--- pr004.java
+++ pr004.java
@@ -6,14 +6,21 @@ class Avtomobil {
 class pr004 {
 	public static void main(String args[]) {
 		Avtomobil lada = new Avtomobil();
+		Avtomobil porshe = new Avtomobil();
 		int rasst;
 
 		lada.pass = 4;
 		lada.v = 40;
 		lada.rash = 9.5;
 
+		porshe.pass = 1;
+		porshe.v = 100;
+		porshe.rash = 14.8;
+
 		rasst = (int) (lada.v/lada.rash *100);
-		
+		rasst2 = (int) (roshe.v/roshe.rash *100);
+
 		System.out.println("Lada проедет " + rasst + "км. на полном баке");
+		System.out.println("Porshe  проедет " + rasst2 + "км. на полном баке");
 	}
 }
